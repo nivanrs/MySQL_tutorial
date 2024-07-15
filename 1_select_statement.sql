7ffdd980299e5fd94f31a7a43a4df90b7eaf037f
nivanrs <nivanrs@gmail.com>
2024-07-15 21:28:54 +0700

    menambahkan bahasa indonesia ke tahap 1 dan 2

--- 1_select_statement.sql
+++ 1_select_statement.sql
@@ -5,12 +5,14 @@ SELECT *
 FROM Parks_and_Recreation.employee_demographics;
 
 -- select several column
+-- Memilih beberapa kolom 
 SELECT first_name,
     last_name,
     birth_date
 FROM Parks_and_Recreation.employee_demographics;
 
 -- Pemdas operation in Select statement, the following will prioritize parenthesis
+-- Select statement mengikuti aturan pemdas, dimana kita memprioritaskan kurung yang paling depan, eksponensial, perkalian, pembagian, penambahan baru kemudian pengurangan
 SELECT first_name,
     last_name,
     birth_date,
@@ -19,5 +21,6 @@ SELECT first_name,
 FROM Parks_and_Recreation.employee_demographics;
 
 -- Select Distinct to keep unique results
+-- Select distict digunakan untuk memilih kolom yang hasilnya unik
 SELECT DISTINCT gender
 FROM Parks_and_Recreation.employee_demographics;
--- 2_where_statement.sql
+++ 2_where_statement.sql
@@ -1,4 +1,5 @@
 -- Basic comparison
+-- Beberapa perbandingan umum dalam SQL adalah lebih besar, lebih kecil, lebih dari sama dengan, kurang dari sama dengan, dan tidak sama dengan.
 SELECT *
 FROM Parks_and_Recreation.employee_salary
 WHERE salary > 50000;
@@ -16,6 +17,7 @@ FROM Parks_and_Recreation.employee_demographics
 WHERE gender != 'Female';
 
 -- Logical statement
+-- Pernyataan logika seperti dan atau atau digunakan disini
 SELECT *
 FROM Parks_and_Recreation.employee_demographics
 WHERE birth_date > '1985-01-01'
@@ -27,12 +29,13 @@ WHERE birth_date > '1985-01-01'
     or gender = 'Female';
 
 -- Like statement
--- Find all names that start with K
+-- Find all names that start with K, menambahkan % peda akhir K berarti memfilter hasil hanya untuk hasil yang memiliki awalan K
 SELECT *
 FROM Parks_and_Recreation.employee_demographics
 WHERE last_name LIKE 'K%';
 
 -- Find name start with a followed by 2 characters
+-- tanda _ menunjukkan satu karakter yang akan diisi, dengan 2 karakter _ berarti ada dua karakter yang harus diisi
 SELECT *
 FROM Parks_and_Recreation.employee_demographics
 WHERE last_name LIKE 'a__';
